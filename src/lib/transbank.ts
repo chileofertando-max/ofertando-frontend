eb876f9d586578d597b1658ce9f6ac7742125735
chileofertando-max <chileofertando@gmail.com>
2026-05-01 16:56:08 -0400

    Update transbank.ts

--- src/lib/transbank.ts
+++ src/lib/transbank.ts
@@ -1,18 +1,35 @@
 import {
   WebpayPlus,
   Options,
-  IntegrationApiKeys,
   Environment,
+  IntegrationApiKeys,
   IntegrationCommerceCodes,
 } from "transbank-sdk";
 
+const transbankEnv = process.env.TRANSBANK_ENV || "integration";
+
 const commerceCode =
-  process.env.TRANSBANK_COMMERCE_CODE || IntegrationCommerceCodes.WEBPAY_PLUS;
-const apiKey = process.env.TRANSBANK_API_KEY || IntegrationApiKeys.WEBPAY;
+  process.env.TRANSBANK_COMMERCE_CODE ||
+  IntegrationCommerceCodes.WEBPAY_PLUS;
+
+const apiKey =
+  process.env.TRANSBANK_API_KEY ||
+  IntegrationApiKeys.WEBPAY;
+
 const environment =
-  process.env.TRANSBANK_ENVIRONMENT === "production"
+  transbankEnv === "production"
     ? Environment.Production
     : Environment.Integration;
+
+if (transbankEnv === "production") {
+  if (!process.env.TRANSBANK_COMMERCE_CODE) {
+    throw new Error("Falta TRANSBANK_COMMERCE_CODE en producción");
+  }
+
+  if (!process.env.TRANSBANK_API_KEY) {
+    throw new Error("Falta TRANSBANK_API_KEY en producción");
+  }
+}
 
 export const webpay = new WebpayPlus.Transaction(
   new Options(commerceCode, apiKey, environment),
